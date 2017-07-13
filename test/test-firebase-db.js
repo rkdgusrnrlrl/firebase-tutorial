e5daf34752d9f089f0f6e8e8071df7dc8f1b263a
HyunKu Kang <rkdgusrnrlrl@gmail.com>
2017-07-13 22:39:55 +0900

    벌크 데이터를 추가로 만듬

--- test/test-firebase-db.js
+++ test/test-firebase-db.js
@@ -19,23 +19,49 @@ var db = admin.database();
 
 describe('fire base 테스트', function() {
     const id = "rkdgusrnrlrl";
-    const value = {
+    const me = {
         id: "rkdgusrnrlrl",
         name: "강현구",
-        age: 31
+        age: 31,
+        des : "고수가 되고 싶은 초보 개발자"
+    };
+    const changju = {
+        id: "changju",
+        name: "박창주",
+        age: 31,
+        des : "vue.js 고수"
+    };
+    const junhwang = {
+        id: "junhwang",
+        name: "이준형",
+        age: 29,
+        des : "그냥 고수"
+    };
+    const jinblog = {
+        id: "jinblog",
+        name: "김청진",
+        age: 31,
+        des : "jin blog 블로거, 하지만 요즘은 코인판에... 역시 개발 고수"
     };
 
+
+
     before(() => {
-        db.ref("/users/"+id).set(value);
+        db.ref("/users/"+me.id).set(me);
+        db.ref("/users/"+changju.id).set(changju);
+        db.ref("/users/"+junhwang.id).set(junhwang);
+        db.ref("/users/"+jinblog.id).set(jinblog);
     });
 
-    it('db 에서 값 확인', (done) => {
+    it('db 에서 id 로 값 확인', (done) => {
         db.ref("/users/"+id)
             .once('value')
             .then((data) => {
-                expect(data.toJSON()).to.deep.equal(value);
+                expect(data.toJSON()).to.deep.equal(me);
                 done();
             })
             .catch(done);
     });
+
+
 });
